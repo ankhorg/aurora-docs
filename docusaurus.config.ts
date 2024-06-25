cd38ea16f3c0d2d54d47ada6082a6910bdc3a664
InkerBot <im@inker.bot>
2024-06-26 07:04:18 +0800

    initial

---
+++ docusaurus.config.ts
@@ -0,0 +1,54 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+import projectConfig from './docs/config.json';
+
+const config: Config = {
+    title: projectConfig.title,
+    tagline: projectConfig.tagline,
+    favicon: 'img/favicon.ico',
+
+    url: projectConfig.url,
+    baseUrl: '/',
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    i18n: {
+        defaultLocale: 'zh-Hans',
+        locales: ['zh-Hans'],
+    },
+
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    routeBasePath: '/',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        navbar: {
+            title: projectConfig.title,
+            items: [
+                {
+                    href: projectConfig.downloadUrl,
+                    label: 'Download',
+                    position: 'right',
+                },
+            ],
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;

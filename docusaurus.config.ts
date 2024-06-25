da9bc1a0f334914ee26147c9ff570e1cacabf13c
InkerBot <im@inker.bot>
2024-06-26 07:33:03 +0800

    move checkout into project

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,7 +1,7 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
-import projectConfig from './docs/config.json';
+import projectConfig from './project/docs/config.json';
 
 const config: Config = {
     title: projectConfig.title,
@@ -24,6 +24,7 @@ const config: Config = {
             'classic',
             {
                 docs: {
+                    path: 'project/docs',
                     routeBasePath: '/',
                 },
                 theme: {
